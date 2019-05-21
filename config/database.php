366b46a6e58e864f25d3b0c2eb21bf0a6785400d
faridsh69 <farid.sh69@gmail.com>
2019-05-21 14:45:32 +0430

    dump dataabse

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => env('DB_DUMP_PATH', null),
+                // 'dump_binary_path' => null,
                 // 'dump_binary_path' => 'C:\xampp\mysql\bin', // only the path, so without `mysqldump` or `pg_dump`
                 // 'use_single_transaction',
                 // 'timeout' => 60 * 5, // 5 minute timeout
