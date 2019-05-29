d476e0361b0aada01eef840398d464f731b86c3a
faridsh69 <farid.sh69@gmail.com>
2019-05-29 19:55:15 +0430

    factory fixed structure

--- app/Base/BaseFactory.php
+++ app/Base/BaseFactory.php
@@ -1,50 +1,60 @@
 <?php
 
+namespace App\Base;
+
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-$models = ['Blog', 'Page'];
-foreach($models as $model)
+class BaseFactory
 {
-    $class_name = 'App\\Models\\' . $model;
-    $model = new $class_name();
-    $columns = $model->getColumns();
+    public $models = ['Blog', 'Page'];
 
-    $factory->define($class_name, function (Faker $faker) use ($columns) {
-        $output = [];
-        foreach($columns as $column){
-            $name = $column['name'];
-            $type = isset($column['type']) ? $column['type'] : '';
-            $rule = isset($column['rule']) ? $column['rule'] : '';
-            $relation = isset($column['relation']) ? $column['relation'] : '';
-            
-            if($rule === 'nullable'){
-                continue;
-            }
-            elseif($name == 'meta_description'){
-                $output[$name] = $faker->realText(100);
-            }
-            elseif($name == 'url'){
-                $output[$name] = 'fake-' . $faker->numberBetween($min = 1000, $max = 900000);
-            }
-            elseif($name == 'content'){
-                $output[$name] = '<h1>Fake h1</h1><h2>Fake h2</h2>' . $faker->realText(400);
-            }
-            elseif($type == 'string'){
-                $output[$name] = 'Fake ' . $faker->realText(50);
-            }
-            elseif($type == 'text'){
-                $output[$name] = 'Fake ' . $faker->realText(400);
-            }
-            elseif($type == '' || $type == 'boolean'){
-                $output[$name] = 1;
-            }
-            elseif($type === 'integer'){
-                $output[$name] = $faker->numberBetween(1000, 9000);
-            }
+    public function index($factory)
+    {
+        foreach($this->models as $model)
+        {
+            $class_name = 'App\\Models\\' . $model;
+            $model = new $class_name();
+            $columns = $model->getColumns();
+
+            $factory->define($class_name, function (Faker $faker) use ($columns) {
+                $output = [];
+                foreach($columns as $column){
+                    $name = $column['name'];
+                    $type = isset($column['type']) ? $column['type'] : '';
+                    $rule = isset($column['rule']) ? $column['rule'] : '';
+                    $relation = isset($column['relation']) ? $column['relation'] : '';
+                    
+                    if($rule === 'nullable'){
+                        continue;
+                    }
+                    elseif($name == 'meta_description'){
+                        $output[$name] = $faker->realText(100);
+                    }
+                    elseif($name == 'url'){
+                        $output[$name] = 'fake-' . $faker->numberBetween($min = 1000, $max = 900000);
+                    }
+                    elseif($name == 'content'){
+                        $output[$name] = '<h1>Fake h1</h1><h2>Fake h2</h2>' . $faker->realText(400);
+                    }
+                    elseif($type == 'string'){
+                        $output[$name] = 'Fake ' . $faker->realText(50);
+                    }
+                    elseif($type == 'text'){
+                        $output[$name] = 'Fake ' . $faker->realText(400);
+                    }
+                    elseif($type == '' || $type == 'boolean'){
+                        $output[$name] = 1;
+                    }
+                    elseif($type === 'integer'){
+                        $output[$name] = $faker->numberBetween(1000, 9000);
+                    }
+                }
+
+                return $output;
+            });
         }
-
-        return $output;
-    });
+    }
 }
 
 // 'name' => $faker->name,
---
+++ database/factories/AppFactory.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Base\BaseFactory;
+
+$base_factory = new BaseFactory();
+$base_factory->index($factory);
