86764348ea7e036e780797890d0cbaaed9105321
faridsh69 <farid.sh69@gmail.com>
2019-06-06 19:15:22 +0430

    base settings

---
+++ config/base-contact.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+	'phone' => '+100000000',
+	'mobile' => '+190000000',
+	'email' => 'farid.sh69@gmail.com',
+	'fax' => '+130000000',
+	'address' => 'US, California, ....',
+	'address' => 'US, California, ....',
+	'telegram' => 'telegram_id',
+	'instagram' => 'instagram_id',
+	'skype' => 'skype_id',
+	'facebook' => 'facebook_id',
+	'twitter' => 'twitter_id',
+	'google' => 'gplus_id',
+];
