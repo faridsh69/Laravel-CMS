d6cff52f0eedfa83a46dd8af089ea9b1df9bca43
faridsh69 <farid.sh69@gmail.com>
2019-06-23 19:04:21 +0430

    google analytics aadded

--- resources/views/layout/front.blade.php
+++ resources/views/layout/front.blade.php
@@ -2,9 +2,7 @@
 <html lang="en" dir="ltr">
 @include('common.front.header')
 <body>
-
 	@yield('content')
-	
 	@include('front.widgets.scripts.' . config('0-developer.theme'))
 	@include('common.front.scripts')
 	@stack('scripts')
