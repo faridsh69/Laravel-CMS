3c067f6834c81ef8cccfe5d15c68003ddf1ab35a
faridsh69 <farid.sh69@gmail.com>
2019-06-25 15:20:05 +0430

    some changes in theme

--- resources/views/common/admin/toolbar.blade.php
+++ resources/views/common/admin/toolbar.blade.php
@@ -12,7 +12,7 @@
 					<span class="m-dropdown__arrow m-dropdown__arrow--center"></span>
 					<div class="m-dropdown__inner">
 						<div class="m-dropdown__header m--align-center" style="background: url(
-							{{ asset(config('0-general.default_user_image')) }}) ; background-size: cover;">
+							{{ asset('upload/images/notification_bg.jpg') }}) ; background-size: cover;">
 							<span class="m-dropdown__header-title">
 								3 New
 							</span>
@@ -130,7 +130,7 @@
 			<li class="m-nav__item m-topbar__user-profile m-topbar__user-profile--img  m-dropdown m-dropdown--medium m-dropdown--arrow m-dropdown--header-bg-fill m-dropdown--align-right m-dropdown--mobile-full-width m-dropdown--skin-light" data-dropdown-toggle="click">
 				<a href="#" class="m-nav__link m-dropdown__toggle">
 					<span class="m-topbar__userpic">
-						<img src="{{ asset('upload/images/user-default.jpg') }}" class="m--img-rounded m--marginless m--img-centered" alt=""/>
+						<img src="{{ asset(config('0-general.default_user_image')) }}" class="m--img-rounded m--marginless m--img-centered" alt=""/>
 					</span>
 					<span class="m-topbar__username m--hide">
 						Nick
@@ -144,7 +144,7 @@
 							url({{ asset('upload/images/user_profile_bg.jpg') }}); background-size: cover;">
 							<div class="m-card-user m-card-user--skin-dark">
 								<div class="m-card-user__pic">
-									<img src="{{ asset('upload/images/user-default.jpg') }}" class="m--img-rounded m--marginless" alt=""/>
+									<img src="{{ asset(config('0-general.default_user_image')) }}" class="m--img-rounded m--marginless" alt=""/>
 								</div>
 								<div class="m-card-user__details">
 									<span class="m-card-user__name m--font-weight-500">
