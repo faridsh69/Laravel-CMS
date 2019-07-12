cd647dd4277efdcdd5fad2ad3365ead9584450fa
faridsh69 <farid.sh69@gmail.com>
2019-07-12 19:12:36 +0430

    all js files converted into 1 file

--- resources/views/front/widgets/scripts/capp.blade.php
+++ resources/views/front/widgets/scripts/capp.blade.php
@@ -1,8 +1,9 @@
-<script src="{{ asset('js/front/capp/jquery-2.2.4.min.js') }}"></script>
+<script src="{{ asset('js/front/capp/app.js') }}"></script>
+
+<!-- <script src="{{ asset('js/front/capp/jquery-2.2.4.min.js') }}"></script>
 <script src="{{ asset('js/front/capp/popper.min.js') }}"></script>
 <script src="{{ asset('js/front/capp/bootstrap.min.js') }}"></script>
 <script src="{{ asset('js/front/capp/plugins.js') }}"></script>
 <script src="{{ asset('js/front/capp/slick.min.js') }}"></script>
 <script src="{{ asset('js/front/capp/footer-reveal.min.js') }}"></script>
-<script src="{{ asset('js/front/capp/active.js') }}"></script>
-
+<script src="{{ asset('js/front/capp/active.js') }}"></script> -->
