d80675a8c9db01d9f03bdcb1d1343c0fd5216f6a
faridsh69 <farid.sh69@gmail.com>
2019-07-12 19:19:07 +0430

    js files combined

--- resources/views/front/widgets/scripts/capp.blade.php
+++ resources/views/front/widgets/scripts/capp.blade.php
@@ -1,9 +1,9 @@
 <script src="{{ asset('js/front/capp/app.js') }}"></script>
 
-<!-- <script src="{{ asset('js/front/capp/jquery-2.2.4.min.js') }}"></script>
-<script src="{{ asset('js/front/capp/popper.min.js') }}"></script>
-<script src="{{ asset('js/front/capp/bootstrap.min.js') }}"></script>
-<script src="{{ asset('js/front/capp/plugins.js') }}"></script>
-<script src="{{ asset('js/front/capp/slick.min.js') }}"></script>
-<script src="{{ asset('js/front/capp/footer-reveal.min.js') }}"></script>
-<script src="{{ asset('js/front/capp/active.js') }}"></script> -->
+<!-- http://www.cms-laravel.com/cdn/js/front/capp/jquery-2.2.4.min.js
+http://www.cms-laravel.com/cdn/js/front/capp/popper.min.js
+http://www.cms-laravel.com/cdn/js/front/capp/bootstrap.min.js
+http://www.cms-laravel.com/cdn/js/front/capp/plugins.js
+http://www.cms-laravel.com/cdn/js/front/capp/slick.min.js
+http://www.cms-laravel.com/cdn/js/front/capp/footer-reveal.min.js
+http://www.cms-laravel.com/cdn/js/front/capp/active.js -->
