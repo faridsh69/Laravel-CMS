2bc1dbe0c449a555ed066c4bd0da07d863d2e18f
faridsh69 <farid.sh69@gmail.com>
2019-07-14 21:08:41 +0430

    all css files come togheter

--- resources/views/front/widgets/styles/capp.blade.php
+++ resources/views/front/widgets/styles/capp.blade.php
@@ -1,6 +1,7 @@
-<link rel="stylesheet" href="{{ asset('/css/front/capp/css/app.css') }}">
+<link rel="stylesheet" href="https://faridsh.b-cdn.net/app.css">
 
 @if(false)
+<link rel="stylesheet" href="{{ asset('/css/front/capp/css/app.css') }}">
 <!-- <link rel="stylesheet" href="{{ asset('/css/front/capp/style.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/front/capp/responsive.css') }}">
  -->
