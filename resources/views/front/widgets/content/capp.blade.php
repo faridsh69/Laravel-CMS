4c112c57461e19ff44b04e860dca544aecee37f7
faridsh69 <farid.sh69@gmail.com>
2019-07-17 12:26:09 +0430

    content sy nergy added

--- resources/views/front/widgets/content/capp.blade.php
+++ resources/views/front/widgets/content/capp.blade.php
@@ -1,3 +1,9 @@
-<section class="special-area bg-white text-center" style="color: #5b32b4; padding:90px 0px;">
-    {!! $page->content !!}
+<section class="special-area bg-white" style="color: #5b32b4; padding:90px 0px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-12"> 
+			    {!! $page->content !!}
+			</div>
+		</div>
+	</div>
 </section>
