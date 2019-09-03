3391a737640f97c6b5f69e90b0b72dcfff404b42
faridsh69 <farid.sh69@gmail.com>
2019-09-03 12:55:57 +0430

    bazzresi added

--- resources/views/front/test/pipe/pipe-home.blade.php
+++ resources/views/front/test/pipe/pipe-home.blade.php
@@ -3,6 +3,19 @@
 <h1 class="page-header text-center">
 	بازارچه الکترونیکی خرید و فروش لوله های فولادی
 </h1>
+<br>
+<br>
+<br>
+درباره ما
+<br>
+<br>
+<br>
+<br>
+توضیحات
+<br>
+<br>	
+<br>	
+<br>	
 @endsection
 
 
