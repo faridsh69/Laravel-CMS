29349cee8d36bfe488ebd056a7a50338b4cb1342
faridsh69 <farid.sh69@gmail.com>
2019-09-16 16:49:56 +0430

    swipe added

--- resources/views/common/shop/header.blade.php
+++ resources/views/common/shop/header.blade.php
@@ -4,7 +4,8 @@
     @endif
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, height=device-height, viewport-fit=cover" />
+
     <meta name="author" content="farid.sh69@gmail.com">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
