bf58a43fbb82e87cfd133a8d8875733deff24247
faridsh69 <farid.sh69@gmail.com>
2019-11-12 15:51:29 +0330

    authorization added to admin panel

--- resources/views/common/admin/sidebar.blade.php
+++ resources/views/common/admin/sidebar.blade.php
@@ -450,8 +450,8 @@
 		data-menu-vertical="true"
 			data-menu-scrollable="false" data-menu-dropdown-timeout="500">
 		<ul class="m-menu__nav m-menu__nav--dropdown-submenu-arrow">
-			@foreach($sidebar as $item)
-			@if($item['type'] == 'item' && Route::has($item['route']))
+		@foreach($sidebar as $item)
+			@if($item['title'] === 'Dashboard')) 
 			<li aria-haspopup="true" class="m-menu__item 
 				@if(Route::currentRouteName() == $item['route']) m-menu__item--active @endif" >
 				<a href="{{ route($item['route']) }}" class="m-menu__link ">
@@ -461,18 +461,19 @@
 							<span class="m-menu__link-text">
 								{{ $item['title'] }}
 							</span>
-							@if(isset($item['badge']))
-							<span class="m-menu__link-badge">
-								<span class="m-badge m-badge--{{ $item['badge']['type'] }}">
-									{{ $item['badge']['count'] }}
-								</span>
-							</span>
-							@endif
 						</span>
 					</span>
 				</a>
 			</li>
-			@elseif($item['type'] == 'submenu')
+			@elseif($item['type'] == 'section')
+			<li class="m-menu__section">
+				<h4 class="m-menu__section-text">
+					{{ $item['title'] }}
+				</h4>
+				<i class="m-menu__section-icon flaticon-more-v3"></i>
+			</li>
+			@else
+			@can('index', 'App\\Models\\' . $item['title'])
 			<li class="m-menu__item m-menu__item--submenu  
 				@if(Request::segment(2) == $item['route']) m-menu__item--open m-menu__item--expanded @endif" aria-haspopup="true" data-menu-submenu-toggle="hover">
 				<a href="javascript:void(0)" class="m-menu__link m-menu__toggle">
@@ -482,13 +483,6 @@
 							<span class="m-menu__link-text">
 								{{ $item['title'] }}
 							</span>
-							@if(isset($item['badge']))
-							<span class="m-menu__link-badge">
-								<span class="m-badge m-badge--{{ $item['badge']['type'] }}">
-									{{ $item['badge']['count'] }}
-								</span>
-							</span>
-							@endif
 							<i class="m-menu__ver-arrow la la-angle-right"></i>
 						</span>
 					</span>
@@ -496,49 +490,7 @@
 				<div class="m-menu__submenu">
 					<span class="m-menu__arrow"></span>
 					<ul class="m-menu__subnav">
-						<li class="m-menu__item  m-menu__item--parent" aria-haspopup="true" >
-							<a href="javascript:void(0)" class="m-menu__link ">
-								<span class="m-menu__link-text">
-									{{ $item['title'] }}
-								</span>
-							</a>
-						</li>
 						@foreach($item['children'] as $submenu_item)
-						@if( isset($submenu_item['type']))
-						<li class="m-menu__item  m-menu__item--submenu
-						@if(Request::segment(3) == $submenu_item['route']) m-menu__item--open m-menu__item--expanded @endif
-						" aria-haspopup="true"  data-menu-submenu-toggle="hover">
-							<a href="javascript::void(0)" class="m-menu__link m-menu__toggle">
-								<i class="m-menu__link-bullet m-menu__link-bullet--dot">
-									<span></span>
-								</i>
-								<span class="m-menu__link-text">
-									{{ $submenu_item['title'] }}
-								</span>
-								<i class="m-menu__ver-arrow la la-angle-right"></i>
-							</a>
-							<div class="m-menu__submenu">
-								<span class="m-menu__arrow"></span>
-								<ul class="m-menu__subnav">
-									@foreach($submenu_item['children'] as $submenu_item_subitem)
-									@if(Route::has($submenu_item_subitem['route']))
-									<li aria-haspopup="true" class="m-menu__item  
-										@if(Route::currentRouteName() == $submenu_item_subitem['route'] ) m-menu__item--active @endif">
-										<a  href="{{ route($submenu_item_subitem['route']) }}" class="m-menu__link ">
-											<i class="m-menu__link-bullet m-menu__link-bullet--dot">
-												<span></span>
-											</i>
-											<span class="m-menu__link-text">
-												{{ $submenu_item_subitem['title'] }}
-											</span>
-										</a>
-									</li>
-									@endif
-									@endforeach
-								</ul>
-							</div>
-						</li>
-						@else
 						@if(Route::has($submenu_item['route']))
 						<li aria-haspopup="true" class="m-menu__item @if(Route::currentRouteName() == $submenu_item['route'] ) m-menu__item--active @endif">
 							<a  href="{{ route($submenu_item['route']) }}" class="m-menu__link ">
@@ -551,20 +503,13 @@
 							</a>
 						</li>
 						@endif
-						@endif
 						@endforeach
 					</ul>
 				</div>
 			</li>
-			@else
-			<li class="m-menu__section">
-				<h4 class="m-menu__section-text">
-					{{ $item['title'] }}
-				</h4>
-				<i class="m-menu__section-icon flaticon-more-v3"></i>
-			</li>
+			@endcan
 			@endif
-			@endforeach
+		@endforeach
 		</ul>
 	</div>
 </div>
