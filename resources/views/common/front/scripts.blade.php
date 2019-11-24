6e497cae300e791f5d9f752f09693ad9661be0b8
faridsh69 <farid.sh69@gmail.com>
2019-11-24 18:27:01 +0330

    crisp added to scripts

--- resources/views/common/front/scripts.blade.php
+++ resources/views/common/front/scripts.blade.php
@@ -1,12 +1,12 @@
 @stack('scripts')
 {!! config('0-developer.scripts') !!}
 
-@if(config('0-general.crisp_id'))
+@if(config('0-general.hotjar_id'))
     <!-- hotjar codes -->
     <script>
         (function(h,o,t,j,a,r){
             h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:{{config('0-general.crisp_id')}},hjsv:6};
+            h._hjSettings={hjid:{{config('0-general.hotjar_id')}},hjsv:6};
             a=o.getElementsByTagName('head')[0];
             r=o.createElement('script');r.async=1;
             r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
