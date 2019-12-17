1a4a6a2a1c5d3c89f488b93138e3b669c46ab013
faridsh69 <farid.sh69@gmail.com>
2019-12-17 15:57:04 +0330

    change some css

--- resources/views/front/themes/2-shop/subscribe.blade.php
+++ resources/views/front/themes/2-shop/subscribe.blade.php
@@ -1,16 +1,5 @@
 <section class="our-monthly-membership section_padding_50 clearfix">
     <div class="container">
-        <div class="row align-items-center">
-            <div class="col-md-6">
-                <div class="membership-description">
-                    <p>
-                    {!! config('setting-general.subscribe_description') !!}
-                    </p>
-                </div>
-            </div>
-            <div class="col-md-6">
-                @include('front.themes.' . config('setting-developer.theme'). '.form')
-            </div>
-        </div>
+        <iframe style="width: 100%; height: 630px" src="https://www.resyos.com/#hs_cos_wrapper_widget_1555583673487"></iframe>
     </div>
 </section>
