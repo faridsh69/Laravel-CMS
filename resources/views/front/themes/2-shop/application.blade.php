0d2e3601905cce2106d6b325ca6a97da12f2bd30
faridsh69 <farid.sh69@gmail.com>
2019-12-18 12:42:51 +0330

    some css changes

--- resources/views/front/themes/2-shop/application.blade.php
+++ resources/views/front/themes/2-shop/application.blade.php
@@ -2,27 +2,28 @@
     $sliders = \App\Models\Slider::where('id', '>', 2)->active()->get();
     $slider_3 = $sliders->first();
 @endphp
-<section class="special-area bg-white section_padding_100" style="margin-top: -200px;">
-    <div class="special_description_area mt-150">
+<section class="special-area bg-white" style="margin-bottom: 100px;">
+    <div class="special_description_area">
         <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-sm-6 offset-sm-1">
+                    <div class="row">
+                        @foreach($sliders as $slider)
+                        <div class="col-12 mt-3">
+                            <div class="special_description_content">
+                                <h4>{{ $slider->title }}</h4>
+                                <p>{!! $slider->description !!}</p>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
+                </div>
+                <div class="col-sm-4 offset-sm-1">
                     <div class="special_description_img">
                         <img src="{{ $slider_3->asset_image }}" class="pull-right" alt="services">
                     </div>
                 </div>
             </div>
-            <div class="row">
-                @foreach($sliders as $slider)
-                <div class="col-sm-6 col-md-4 ml-xl-auto mt-3">
-                    <div class="special_description_content">
-                        <h4>{{ $slider->title }}</h4>
-                        <p>{!! $slider->description !!}</p>
-                    </div>
-                </div>
-                @endforeach
-            </div>
-            <div class="separate"></div>
         </div>
     </div>
 </section>
