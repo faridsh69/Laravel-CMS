8bb63e932b9c78ebe0ae54848ef14c0e9da39439
faridsh69 <farid.sh69@gmail.com>
2019-12-19 21:19:12 +0330

    change form in theme

--- resources/views/front/themes/1-original/form.blade.php
+++ resources/views/front/themes/1-original/form.blade.php
@@ -7,9 +7,9 @@
 
     <!-- <form action="{{ route('front.page.subscribe') }}" method="post" class="form-inline"> -->
         @csrf
-        <div class="get-started-button wow bounceInDown" data-wow-delay="0.5s">
+        <div class="get-started-button wow bounceInDown" data-wow-delay="0.5s" style="text-align: left">
             <!-- <input type="text" class="form-control email" placeholder="{{ __('phone') }}" name="phone" style="background-color: white"> -->
-            <a target="_blank" href="tel:{{ config('setting-contact.mobile') }}" title="call">
+            <a target="_blank" type="submit" href="tel:{{ config('setting-contact.mobile') }}" title="call">
                 {{ config('setting-contact.mobile') }}
             </a>
             <!-- <input type="submit" class="submit" value="{{ __('start') }}" style="min-width: 140px"> -->
