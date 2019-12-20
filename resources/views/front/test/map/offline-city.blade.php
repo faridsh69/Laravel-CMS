ee7f7270f3e84950522914c3b4aaa6035f957f54
faridsh69 <farid.sh69@gmail.com>
2019-12-20 21:37:52 +0330

    offline map

--- resources/views/front/test/map/offline-city.blade.php
+++ resources/views/front/test/map/offline-city.blade.php
@@ -15,7 +15,7 @@
             startTs: Date.now()
         };
     </script>
-    <meta name='minifest-ecmwf-hres' content='{"v":"2.0","ref":"2019-12-02T09:00:00Z","update":"2019-12-02T09:00:00Z","dst":[[3,3,144],[6,150,240]]}'>
+    <meta name='minifest-ecmwf-hres' content='{"v":"2.0","ref":"2019-12-14T09:00:00Z","update":"2019-12-14T09:00:00Z","dst":[[3,3,144],[6,150,240]]}'>
     <!-- <meta name='minifest-ecmwf-hres' content='{"v":"2.0","ref":"2019-11-26T00:00:00Z","update":"2019-11-26T07:34:48Z","dst":[[3,3,144],[6,150,240]]}'> -->
     <!-- <meta name='minifest-ecmwf-hres' content='{"v":"2.0","ref":"2019-06-21T12:00:00Z","update":"2019-06-21T19:34:37Z","dst":[[3,3,144],[6,150,240]]}'> -->
     <meta name="model" content="ecmwf">
