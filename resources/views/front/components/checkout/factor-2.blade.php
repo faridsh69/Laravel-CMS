a72dde70d2f30b44a0733beb473e3a3e2bdb84d6
faridsh69 <farid.sh69@gmail.com>
2020-02-28 00:32:18 +0330

    invoice

--- resources/views/front/components/checkout/factor-2.blade.php
+++ resources/views/front/components/checkout/factor-2.blade.php
@@ -1,93 +1,84 @@
 <div class="container text-center">
-	<h5>This is the place for invoice</h5>
+	<h5>This is the place for html invoice</h5>
 	<div class="row">
-		<div class="col-md-6 offset-md-3">
-				<div class="col-12">
-					<div class="card card-success">
-						<div class="card-header">
-							Factor 
-						</div>
-				    	<div class="card-body">
-				    		<div class="row">
-					    		<div class="col-sm-6">
-						    		<label>Number:</label> 29
-									<div class="one-third-seperate"></div>
-									<label>Shipping:</label> 
-									<div class="one-third-seperate"></div>
-									<label>
-									Date: 
-									</label> 
-									02/27/2020
-								</div>
-								<div class="col-sm-6">
-									California, livermore
-								</div>
-							</div>	
+		<div class="col-md-8 offset-md-2">
+			<div class="row">
+				<img src="{{ asset('images/front/general/top-invoice.png') }}" style="width: 100%">
+				<div class="col-12" style="height: 500px; background: linear-gradient(#f2f0f1, white);">
+				<!-- baldvin ringsted -->
+					<div class="row">
+						<div class="col-10 offset-1">
 							<div class="row">
-								<div class="col-sm-6">
-									<table class="table table-striped table-hover">
-									<thead>
-									<tr>
-										<th>
-											Count	
-										</th>
-										<th>
-											Name
-										</th>
-										<th>
-											Fee
-										</th>
-										<th>
-											Discount
-										</th>
-									</tr>
-									</thead>
-									<tbody>
-									
-									<tr>
-										<td>
-											1
-										</td>
-										<td>
-											product #931
-										</td>
-										<td>
-											1000
-										</td>
-										<td>
-											900
-										</td>
-									</tr>
-
-									</tbody>
+								<div class="col-5">
+									<img src="{{ asset('images/front/general/invoice-data.png') }}">
+								</div>
+								<div class="col-4 offset-3">
+									<p style="font-size: 30px; color: #ccc">INVOICE</p>
+									<span>DATE</span>
+									<span>March 03, 2020</span>
+									<span>INVOICE#</span>
+									<span>INV302</span>
+								</div>
+								<div class="col-12">
+									<hr style="background: rgb(175, 198, 229); height: 2px;">
+								</div>
+								<div class="col-6">
+									<span class="invoice-label">Bill To:</span>
+									216 Duffield St, Brooklyn, NY 11201, United States
+								</div>
+								<div class="col-6">
+									<span class="invoice-label">Ship To:</span>
+									216 Duffield St, Brooklyn, NY 11201, United States
+								</div>
+								<div class="col-12">
+									<table class="table mt-5">
+										<thead>
+											<th>
+												Product ID
+											</th>
+											<th>
+												Description
+											</th>
+											<th>
+												Quantity
+											</th>
+											<th>
+												Unit Price
+											</th>
+											<th>
+												Line Total
+											</th>
+										</thead>
+										<tbody>
+											<tr>
+												<td>
+													P145
+												</td>
+												<td>
+													Art Work Fall
+												</td>
+												<td>
+													1
+												</td>
+												<td>
+													9700.00
+												</td>
+												<td>
+													9700.00
+												</td>
+											</tr>
+										</tbody>
 									</table>
 								</div>
-								<div class="col-sm-6">
-									<dl class="dl-horizontal">
-									  	<dt class="big-size">Total</dt>
-									  	<dd class="big-size">1320 $</dd>
-									  	
-
-										  	<div class="half-seperate"></div>
-										  	<dt>Tax</dt>
-										  	<dd>200 $</dd>
-									  	
-
-									  	<div class="seperate"></div>
-									  	<div class="seperate"></div>
-									  	<dt class="double-size">Total Price</dt>
-									  	<dd class="double-size">
-									  		1520 $</dd>
-									</dl>
-								</div>
 							</div>
-				    	</div>
-				    </div>
+						</div>
+					</div>
 				</div>
+			</div>
 		</div>
 	</div>
 	<hr>
-	<h5>This is the image of invoice</h5>
+	<h5>This is the image of sample invoice</h5>
 	<br>
 	<br>
 	<!-- <img src="{{ asset('images/front/general/sample-invoice-7.png') }}" class="factor-image-2" alt="invoice"> -->
@@ -107,6 +98,13 @@
 	max-width: 100% !important;
 	width: 400px;
 	margin: 0px auto;
+}
+.invoice-label{
+	display: inline-block;
+	color: white;
+	padding: 5px;
+	border-radius: 3px;
+	background: linear-gradient(141deg, #9fb8ad 0%, #1fc8db 51%, #2cb5e8 75%);
 }
 </style>
 @endpush
