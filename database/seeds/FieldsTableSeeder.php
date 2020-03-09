4db5e3c4a476ed6abd5e9827a279407f17ec3623
faridsh69 <farid.sh69@gmail.com>
2020-03-09 13:09:23 +0330

    creating document for cms-laravel

--- database/seeds/FieldsTableSeeder.php
+++ database/seeds/FieldsTableSeeder.php
@@ -164,7 +164,7 @@ class FieldsTableSeeder extends Seeder
         ]);
 
         $job_apply_form->fields()->sync([
-        	1,2,3,4,5,6,7,8,9,11,12,15,16,17,18
+        	1,2,3,4,5,6,7,8,9,10,11,12,13,14,15
         ], true);
     }
 }
