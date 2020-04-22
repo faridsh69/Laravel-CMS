6342eb3d2cfc95ae4a4358730540ec91c4645e5f
faridsh69 <farid.sh69@gmail.com>
2020-04-22 12:37:06 +0430

    controllers extend one controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -3,26 +3,36 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Services\BaseListController;
-use Illuminate\Http\Request;
 use Kris\LaravelFormBuilder\FormBuilder;
 
-class AdminController
+$model_name = ucfirst(\Request::segment(2));
+$controller_file = __DIR__ . '/' . $model_name . '/ResourceController.php';
+class AdminResourceController extends BaseListController{}
+if (file_exists($controller_file)) {
+	$model_controller = 'App\\Http\\Controllers\\Admin\\' . $model_name . '\\ResourceController';
+	$code =<<<EOF
+  		class AdminResourceController extends $model_controller {}
+EOF;
+	eval( $code );
+}
+
+class AdminController extends AdminResourceController
 {
-	public function __construct(Request $request, FormBuilder $form_builder)
-    {
-        $model_name = ucfirst($request->segment(2));
-        $model_controller = 'App\\Http\\Controllers\\Admin\\' . $model_name . '\\ResourceController';
+	// public function __construct(Request $request, FormBuilder $form_builder)
+ //    {
+ //        $model_name = ucfirst($request->segment(2));
+        
 
-        $controller_file = __DIR__ . '/' . $model_name . '/ResourceController.php';
-        if(file_exists($controller_file)){
-            $controller = new $model_controller($request, $form_builder);
-        } else{
-        	$controller = new BaseListController($request, $form_builder);
-        }
+ //        $controller_file = __DIR__ . '/' . $model_name . '/ResourceController.php';
+ //        if(file_exists($controller_file)){
+ //            $controller = new $model_controller($request, $form_builder);
+ //        } else{
+ //        	$controller = new BaseListController($request, $form_builder);
+ //        }
 
-        $action = \Route::currentRouteAction();
-        list($route_controller, $method) = explode('@', $action);
+ //        $action = \Route::currentRouteAction();
+ //        list($route_controller, $method) = explode('@', $action);
 
-        return $controller->{$method}();
-    }
+ //        return $controller->{$method}();
+ //    }
 }
