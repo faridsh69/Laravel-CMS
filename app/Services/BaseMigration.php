a7152be8d45e8fc65aa59008a718eb81c5aa4927
faridsh69 <farid.sh69@gmail.com>
2020-04-24 23:33:40 +0430

    migration with update and backup and GG

--- app/Services/BaseMigration.php
+++ app/Services/BaseMigration.php
@@ -5,78 +5,86 @@ namespace App\Services;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Str;
 
 class BaseMigration extends Migration
 {
     // models of migration tables
-    public $models;
+    public $model_slugs = [];
 
     // model of migration table
-    public $model;
+    public $model_slug;
 
-    // drop and rebuild table
-    public $rebuild = false;
+    // if table exist it will update or rebuild
+    public $update = true;
 
-    // create backup before rebuild
+    // create backup before update or rebuild
     public $backup = false;
 
-    // seed data after rebuild
-    public $seed = false;
+    private $_migrations = [];
 
     public function __construct()
     {
-        $class_name = 'App\\Models\\' . $this->model;
-        $model = new $class_name();
-        $this->table_name = $model->getTable();
-        $this->columns = $model->getColumns();
+        if($this->model_slugs === []){
+            $this->model_slugs = [$this->model_slug];
+        }
+        foreach($this->model_slugs as $model_slug){
+            $model_name = Str::studly($model_slug);
+            $model_namespace = config('cms.config.models_namespace'). $model_name;
+            $model_repository = new $model_namespace;
+            $model_columns = $model_repository->getColumns();
+            $model_table = $model_repository->getTable();
+            $this->_migrations[] = (object) [
+                'model_slug' => $model_slug,
+                'model_name' => $model_name,
+                'model_namespace' => $model_namespace,
+                'model_repository' => $model_repository,
+                'model_columns' => $model_columns,
+                'model_table' => $model_table,
+            ];
+        }
     }
 
     public function up()
     {
-        $columns = $this->columns;
-        $table_name = $this->table_name;
-
-        Schema::defaultStringLength(191);
-        if($this->rebuild === true){
-            $model_namespance = 'App\\Models\\' . $this->model;
-            $repository = new $model_namespance();
-            $count = $repository::count();
-            if($count > 0){
+        Schema::defaultStringLength(191); // if you are using mariaDB you need this.
+        foreach($this->_migrations as $_migration)
+        {
+            if(Schema::hasTable($_migration->model_table) === false){
+                $this->_createMigration($_migration->model_table, $_migration->model_columns);
+            }else{
                 if($this->backup === true){
-                    $backup_data = $repository::withTrashed()
-                        ->get()
-                        ->makeVisible('deleted_at')
-                        ->toArray();
-                    $backup_table_name = $table_name . '_backup_' . strtotime('now');
-                    Schema::create($backup_table_name, function (Blueprint $table) {
-                        $table->bigIncrements('id');
-                        $table->text('row_data')->nullabe();
-                        $table->timestamps();
-                        $table->softDeletes();
-                    });
-                    foreach($backup_data as $backup_item){
-                        \DB::table($backup_table_name)->insert([
-                            'id' => $backup_item['id'],
-                            'created_at' => $backup_item['created_at'],
-                            'updated_at' => $backup_item['updated_at'],
-                            'deleted_at' => $backup_item['deleted_at'],
-                            'row_data' => serialize($backup_item),
-                        ]);
-                    }
+                    $this->_createBackupTable($_migration->model_table, $_migration->model_repository);
+                }
+                if($this->update === true){
+                    $this->_updateMigration($_migration->model_table, $_migration->model_columns);
                 }else{
-                    dump('Model records count: ' . $repository::count());
-                    dump('Your data will be destroyes, You can pause the proccess with Ctrl + C, you have 5 seconds to do that!');
-                    sleep(6);
+                    $this->_dropTable($_migration->model_table);
+                    $this->_createMigration($_migration->model_table, $_migration->model_columns);
                 }
             }
-            Schema::dropIfExists($this->table_name);
         }
-        if(Schema::hasTable($table_name) === true){
-            return false;
+    }
+
+    public function down()
+    {
+        $reversed_migrations = collect($this->_migrations)->reverse();
+        foreach($reversed_migrations as $_migration)
+        {
+            $this->_dropTable($_migration->model_table);
         }
-        Schema::create($table_name, function (Blueprint $table) use ($columns, $table_name) {
+    }
+
+    private function _dropTable($model_table)
+    {
+        Schema::dropIfExists($model_table);
+    }
+
+    private function _createMigration($model_table, $model_columns)
+    {
+        Schema::create($model_table, function (Blueprint $table) use ($model_columns, $model_table) {
             $table->bigIncrements('id');
-            foreach($columns as $column){
+            foreach($model_columns as $column){
                 $name = $column['name'];
                 $type = isset($column['type']) ? $column['type'] : '';
                 $database = isset($column['database']) ? $column['database'] : '';
@@ -85,17 +93,14 @@ class BaseMigration extends Migration
                 if($database === 'none'){
                     continue;
                 }
+                $table->{$type}($name)->{$database}(true);
                 // for create foreign key relation attribute should be defined
-                elseif($relation){
-                    $table->{$type}($name)->{$database}();
+                if($relation){
                     $table->foreign($name)->references('id')->on($relation);
-                }
-                else{
-                    $table->{$type}($name)->{$database}(true);
                 }
             }
             // for users table need to have rememberToken
-            if($table_name === 'users'){
+            if($model_table === 'users'){
                 $table->rememberToken();
             }
             // for all tables timestamps and softDelete created
@@ -104,10 +109,66 @@ class BaseMigration extends Migration
         });
     }
 
-    public function down()
+    private function _updateMigration($model_table, $model_columns)
     {
-        if($this->rebuild === false){
-            Schema::dropIfExists($this->table_name);
+        $old_database_columns = Schema::getColumnListing($model_table);
+        $extra_columns = ['id', 'created_at', 'updated_at', 'deleted_at'];
+        $drop_columns = $old_database_columns;
+        $add_columns = $model_columns;
+        foreach($old_database_columns as $column_key => $old_database_column){
+            $array_index = array_search($old_database_column, collect($model_columns)->pluck('name')->toArray());
+            if($array_index !== false){
+                unset($drop_columns[$column_key]);
+                unset($add_columns[$array_index]);
+            }
+            if(array_search($old_database_column, $extra_columns) !== false){
+                unset($drop_columns[$column_key]);
+            }
+        }
+        Schema::table($model_table, function (Blueprint $table) use ($add_columns, $drop_columns) {
+            foreach($drop_columns as $drop_column){
+                if(strpos($drop_column, '_id') !== false){
+                    $table->dropForeign([$drop_column]);
+                }
+                $table->dropColumn($drop_column);
+            }
+            foreach($add_columns as $column){
+                $name = $column['name'];
+                $type = isset($column['type']) ? $column['type'] : '';
+                $database = isset($column['database']) ? $column['database'] : '';
+                $relation = isset($column['relation']) ? $column['relation'] : '';
+                if($database === 'none'){
+                    continue;
+                }
+                $table->{$type}($name)->{$database}(true)->after('id');
+                if($relation){
+                    $table->foreign($name)->references('id')->on($relation);
+                }
+            }
+        });
+    }
+
+    private function _createBackupTable($model_table, $model_repository)
+    {
+        $model_repository_list = $model_repository->withTrashed()
+            ->get()
+            ->makeVisible('deleted_at')
+            ->toArray();
+        $backup_table_name = $model_table. '_backup_'. strtotime('now');
+        Schema::create($backup_table_name, function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('row_data')->nullabe();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+        foreach($model_repository_list as $model_repository_item){
+            \DB::table($backup_table_name)->insert([
+                'id' => $model_repository_item['id'],
+                'created_at' => $model_repository_item['created_at'],
+                'updated_at' => $model_repository_item['updated_at'],
+                'deleted_at' => $model_repository_item['deleted_at'],
+                'row_data' => serialize($model_repository_item),
+            ]);
         }
     }
 }
