e4b8e25c5bf0f546aa6de70d33a6f1d67706dd70
faridsh69 <farid.sh69@gmail.com>
2020-05-12 06:32:36 +0430

    fix seeder

--- app/Services/BaseResourceController.php
+++ app/Services/BaseResourceController.php
@@ -48,6 +48,7 @@ class BaseResourceController extends BaseAdminController
         $this->model_repository = new $this->model_namespace;
         $this->model_columns = $this->model_repository->getColumns();
         $this->model_form = 'App\Forms\\'. $this->model_name. 'Form';
+        dd($this->model_form);
         if(!file_exists(__DIR__. '\..\..\\'. $this->model_form. '.php')){
             $this->model_form = 'App\Forms\Form';
         }
