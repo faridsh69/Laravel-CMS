8c199b8be7c172c791606bd2ec5be628287a6cbb
faridsh69 <farid.sh69@gmail.com>
2020-07-25 20:39:19 +0430

    list show content in html rendering

--- resources/views/front/theme/classic/list-item.blade.php
+++ resources/views/front/theme/classic/list-item.blade.php
@@ -2,6 +2,7 @@
     <div class="rtl-text-right single-top-popular-course d-flex align-items-center flex-wrap mb-30 wow fadeInUp" data-wow-delay="500ms">
         <div class="popular-course-content">
             <h5>{{ $item->title }}</h5>
+            @if(false)
             <span>By Simon Smith   |  {{ $item->created_at }}</span>
             <div class="course-ratings">
                 <i class="fa fa-star" aria-hidden="true"></i>
@@ -10,7 +11,8 @@
                 <i class="fa fa-star" aria-hidden="true"></i>
                 <i class="fa fa-star-o" aria-hidden="true"></i>
             </div>
-            <p>{{ $item->content }}</p>
+            @endif
+            <p>{!! $item->content !!}</p>
             <a href="{{ route('front.'. Str::kebab(class_basename($item)). '.show', $item->url) }}" class="btn academy-btn btn-sm">{{ __('See More') }}</a>
         </div>
         <div class="popular-course-thumb bg-img" style="background-image: url({{ $item->src('image') }});"></div>
