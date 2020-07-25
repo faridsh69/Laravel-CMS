dddbcfbd4acdb7d70fced7c47a9f433670358c28
faridsh69 <farid.sh69@gmail.com>
2020-07-25 19:54:47 +0430

    show list in front end removed

--- resources/views/admin/list/show.blade.php
+++ resources/views/admin/list/show.blade.php
@@ -18,9 +18,11 @@
 							{{ __('Check user identify') }}
 						</a>
 					@endif
+					@if(false)
 					<a href="{{ route('front.'. Str::kebab(class_basename($data)) .'.show', $data['url']) }}" target="_blank" class="btn btn-danger btn-sm m-btn m-btn--custom m-btn--air m-btn--pill">
 						{{ __('Show at website') }}
 					</a>
+					@endif
 				</h3>
 			</div>
 		</div>
