12cdf2a84adcad11bb950fc52ceb77a278119f3f
faridsh69 <farid.sh69@gmail.com>
2020-07-25 20:38:17 +0430

    list show content in html rendering

--- resources/views/front/theme/classic/list-item-2.blade.php
+++ resources/views/front/theme/classic/list-item-2.blade.php
@@ -7,10 +7,12 @@
         <!-- Post Title -->
         <a href="{{ route('front.'. Str::kebab(class_basename($item)). '.show', $item->url) }}" class="post-title">{{ $item->title }}</a>
         <!-- Post Meta -->
+        @if(false)
         <div class="post-meta">
             <p>By <a href="#">Simon Smith</a> | <a href="#">{{ $item->created_at }}</a> | <a href="#">3 comments</a></p>
         </div>
-        <p>{{ $item->content }}</p>
+        @endif
+        <p>{!! $item->content !!}</p>
         <a href="{{ route('front.'. Str::kebab(class_basename($item)). '.show', $item->url) }}" class="btn academy-btn btn-sm mt-15">{{ __('Read More')}}</a>
     </div>
 </div>
